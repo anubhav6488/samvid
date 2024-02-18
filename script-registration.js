79016fd8662cf94c956a59e8d203106540e78168
Anubhav Pratap Singh <anubhav6488@gmail.com>
2024-02-18 22:26:44 +0530

    time addded

--- script-registration.js
+++ script-registration.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add event listener to the submit button
     submitBtn.addEventListener("click", function(e) {
         e.preventDefault();
+        const currentDate = new Date();
 
+        // Extract day and month components
+        const day = String(currentDate.getDate()).padStart(2, '0'); // Ensure 2 digits
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+
+        // Construct formatted date string and store it in a variable
+        const Time = `${day} / ${month}`;
         // Get the values of the input fields
         var firstName = document.getElementById('First_Name').value;
         var lastName = document.getElementById('Last_Name').value;
@@ -20,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         formData.append('Number', number);
         formData.append('email', email);
         formData.append('token', token);
+        formData.append('Time', Time);
 
         // Send the data using fetch
         fetch('https://sheetdb.io/api/v1/7xew1pphufyj5', {
